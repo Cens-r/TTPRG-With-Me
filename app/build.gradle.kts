e13656aafc9ed0cd55189e3518595ebcee678c4d
Censor <22554514+Cens-r@users.noreply.github.com>
2023-09-14 09:44:46 -0400

    Updated TargetSDK Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "edu.floridapoly.mobiledeviceapps.fall23.team10.ttrpg_with_me"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "edu.floridapoly.mobiledeviceapps.fall23.team10.ttrpg_with_me"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
