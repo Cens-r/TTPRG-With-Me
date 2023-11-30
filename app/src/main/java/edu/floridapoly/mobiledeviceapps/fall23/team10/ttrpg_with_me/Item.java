6a5192050d3d46e79776cb07783583b0a4bf0ce8
Censor <22554514+Cens-r@users.noreply.github.com>
2023-11-30 05:09:16 -0500

    Split threads for OpenAI requests

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/team10/ttrpg_with_me/Item.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/team10/ttrpg_with_me/Item.java
@@ -1,5 +1,7 @@
 package edu.floridapoly.mobiledeviceapps.fall23.team10.ttrpg_with_me;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import org.json.JSONException;
@@ -13,6 +15,8 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Item extends ClassManager {
     private final static String API_KEY = "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";
@@ -48,35 +52,48 @@ public class Item extends ClassManager {
         return this;
     }
 
-    public static Item Generate(String type, Character character) throws IOException, JSONException {
-        HttpURLConnection connection = GetConnection();
+    public static Item Generate(String type, Character character) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
 
-        String formattedPrompt = String.format(ITEM_PROMPT, type, 5, "human", "bard");
-        String requestBody = String.format(REQUEST_PROMPT, AI_MODEL, formattedPrompt);
-        connection.setDoOutput(true);
+        executor.execute(() -> {
+            HttpURLConnection connection = null;
+            String itemName, itemDescription;
+            try {
+                connection = GetConnection();
 
-        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-        writer.write(requestBody);
-        writer.flush();
-        writer.close();
+                String formattedPrompt = String.format(ITEM_PROMPT, type, 5, "human", "bard");
+                String requestBody = String.format(REQUEST_PROMPT, AI_MODEL, formattedPrompt);
+                connection.setDoOutput(true);
 
-        String responseText;
+                OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+                writer.write(requestBody);
+                writer.flush();
+                writer.close();
 
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String output;
-            StringBuffer response = new StringBuffer();
-            while ((output = br.readLine()) != null) {
-                response.append(output);
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String output;
+                    StringBuffer response = new StringBuffer();
+                    while ((output = br.readLine()) != null) {
+                        response.append(output);
+                    }
+                    br.close();
+
+                    Log.d("Response", response.toString());
+
+                    //JSONObject responseJson = new JSONObject(response.toString());
+                    //responseText = responseJson.getString("content");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-            br.close();
 
-            Log.d("Response", response.toString());
-
-            //JSONObject responseJson = new JSONObject(response.toString());
-            //responseText = responseJson.getString("content");
-        }
+            handler.post(() -> {
+                // Generate an item here if itemName and itemDescription
+            });
+        });
 
         return null;
     }
