8db7f213298d144d6f070c20a6f08e651fcf05f8
Censor <22554514+Cens-r@users.noreply.github.com>
2023-11-30 03:33:31 -0500

    Described ClassManager with comments

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/team10/ttrpg_with_me/ClassManager.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/team10/ttrpg_with_me/ClassManager.java
@@ -9,10 +9,12 @@ public class ClassManager {
     private static Hashtable<String, Hashtable<String, ClassManager>> classObjects = new Hashtable<>();
     public UUID uuid;
 
+    // Constructor
     public ClassManager() {
         uuid = UUID.randomUUID();
     }
 
+    // Starts tracking the given object
     protected static void trackObject(ClassManager object) {
         String className = object.getClass().getSimpleName();
         Hashtable<String, ClassManager> objectList;
@@ -25,16 +27,20 @@ public class ClassManager {
         objectList.put(object.uuid.toString(), object);
     }
 
+    // Gets a hashtable of all the objects for a class
     public static Hashtable<String, ClassManager> getObjects(Class classRef) {
         String className = classRef.getSimpleName();
         return classObjects.get(className);
     }
+
+    // Gets an object given its class and uuid
     public static ClassManager getObject(Class classRef, String uuid) {
         Hashtable<String, ClassManager> objectList = getObjects(classRef);
         if (objectList == null) { return null; }
         return objectList.get(uuid);
     }
 
+    // Converts an object to Json String
     public String toJson() {
         Gson gson = new Gson();
         return gson.toJson(this);
