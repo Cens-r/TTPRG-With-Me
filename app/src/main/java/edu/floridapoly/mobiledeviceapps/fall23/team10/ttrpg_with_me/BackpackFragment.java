8d13a21b836db843fb0b75806bba9ee6ed828c42
Censor <22554514+Cens-r@users.noreply.github.com>
2023-11-29 22:27:53 -0500

    Created Backpack UI

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/team10/ttrpg_with_me/BackpackFragment.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall23/team10/ttrpg_with_me/BackpackFragment.java
@@ -1,40 +1,27 @@
 package edu.floridapoly.mobiledeviceapps.fall23.team10.ttrpg_with_me;
 
+import android.content.ClipData;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BackpackFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BackpackFragment extends Fragment {
-    public BackpackFragment() {
-        // Required empty public constructor
-    }
+import java.util.ArrayList;
+import java.util.List;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Backpack.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BackpackFragment newInstance(String param1, String param2) {
-        BackpackFragment fragment = new BackpackFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class BackpackFragment extends Fragment {
+    String[] nameList = {"Weapons", "Spells", "Armor", "Items", "Extras"};
+    List<ItemContainer> itemContainers;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,22 +33,57 @@ public class BackpackFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_backpack, container, false);
 
-        Button button1 = (Button) rootView.findViewById(R.id.button);
-        Button button2 = (Button) rootView.findViewById(R.id.button2);
-        Button button3 = (Button) rootView.findViewById(R.id.button3);
-        Button button4 = (Button) rootView.findViewById(R.id.button4);
+        ViewGroup linearLayout = rootView.findViewById(R.id.backpack_linear_container);
+        for (int index = 0; index < linearLayout.getChildCount(); index++) {
+            ItemContainer item = new ItemContainer(linearLayout, nameList[index]);
+            itemContainers.add(item);
+        }
+        return rootView;
+    }
 
-        Button[] buttonArray = {button1, button2, button3, button4};
-        for (int i = 0; i < buttonArray.length; i++) {
-            buttonArray[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(v.getContext(), "Will open up more info about the item!", Toast.LENGTH_SHORT).show();
+    public static class ItemContainer {
+        boolean isExpanded;
+        List<View> itemList;
+
+        View container;
+        TextView headerText;
+        ImageButton generateButton;
+        ImageButton arrowButton;
+
+        public ItemContainer(View container, String name) {
+            isExpanded = false;
+
+            this.container = container;
+            headerText = container.findViewById(R.id.item_text_header);
+            generateButton = container.findViewById(R.id.item_button_generate);
+            arrowButton = container.findViewById(R.id.item_button_arrow);
+
+            headerText.setText(name);
+            generateButton.setOnClickListener(view -> {
+                String itemName, itemDescription;
+                // TODO: Make AI request and get itemName and itemDescription
+                View item = this.createItem("Test", "Test description here...");
+                itemList.add(item);
+            });
+            arrowButton.setOnClickListener(view -> {
+                ViewGroup layout = container.findViewById(R.id.item_linear_body);
+                for (int index = 0; index < layout.getChildCount(); index++) {
+                    View element = layout.getChildAt(index);
+                    element.setVisibility((isExpanded) ? View.GONE : View.VISIBLE);
                 }
+                isExpanded = !isExpanded;
             });
         }
 
-        // Inflate the layout for this fragment
-        return rootView;
+        View createItem(String name, String description) {
+            ViewGroup layout = container.findViewById(R.id.item_linear_body);
+            LayoutInflater inflater = LayoutInflater.from(layout.getContext());
+            View view = inflater.inflate(R.layout.display_container, layout, false);
+            if (!isExpanded) {
+                view.setVisibility(View.GONE);
+            }
+            layout.addView(view);
+            return view;
+        }
     }
 }
